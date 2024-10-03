797f0633f776500d09fa70b361379b53a3de959a
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-10-03 14:06:00 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 import { Header } from "../Header/Header";
-import { Footer } from "module";
+import { Footer } from "../Footer/Footer";
 
 export const Layout = () => {
   return (
